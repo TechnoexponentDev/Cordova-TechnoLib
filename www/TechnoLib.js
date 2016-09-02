c90dc1008900419da027bc28da0b366f1a8489d4
TechnoexponentDev <test@technoexponent.com>
2016-09-02 15:14:37 +0530

    Update TechnoLib.js

--- www/TechnoLib.js
+++ www/TechnoLib.js
@@ -1,11 +1,4 @@
-var argscheck = require('cordova/argscheck'),
-channel = require('cordova/channel'),
-exec = require('cordova/exec');
-
-channel.createSticky('onCordovaInfoReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
-
+var exec = require('cordova/exec');
 
 function successCallBack(){
 	console.log('Technolib success.');
@@ -15,16 +8,14 @@ function failureCallBack(){
 	console.log('Technolib failure.');
 }
 
-TechnoLib.technoAlert = function(title,message) {
-	argscheck.checkArgs('fF', 'TechnoLib.technoAlert', arguments);
+exports.technoAlert = function(title,message) {
 	exec(successCallback, errorCallback, "TechnoLib", "technoAlert", [title,message]);
 };
 
-TechnoLib.getUDID = function() {
-	argscheck.checkArgs('fF', 'TechnoLib.getUDID', arguments);
+exports.getUDID = function() {
 	return exec(successCallback, errorCallback, "TechnoLib", "getUDID", []);
 };
 
 
-module.exports = TechnoLib;
+
 
