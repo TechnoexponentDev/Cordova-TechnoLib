ce52427b62a9e1d7cdf1b380de33c904a48d9994
TechnoexponentDev <test@technoexponent.com>
2016-09-02 13:53:35 +0530

    Add files via upload

---
+++ TechnoLib.js
@@ -0,0 +1,29 @@
+var argscheck = require('cordova/argscheck'),
+channel = require('cordova/channel'),
+exec = require('cordova/exec');
+
+channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
+
+
+function successCallBack(){
+	console.log('Technolib success.');
+}
+
+function failureCallBack(){
+	console.log('Technolib failure.');
+}
+
+TechnoLib.technoAlert = function(title,message) {
+	argscheck.checkArgs('fF', 'TechnoLib.technoAlert', arguments);
+	exec(successCallback, errorCallback, "TechnoLib", "technoAlert", [title,message]);
+};
+
+TechnoLib.getUDID = function() {
+	argscheck.checkArgs('fF', 'TechnoLib.getUDID', arguments);
+	return exec(successCallback, errorCallback, "TechnoLib", "getUDID", []);
+};
+
+
+module.exports = TechnoLib;
